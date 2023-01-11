bd558b8abe1e31cfba756c76fcb1ac65ff9e6c88
MiguelX413 <lbpgaming27@gmail.com>
2023-01-10 17:54:34 -0800

    Make working version

--- arduino-jeopardy-buttons.ino
+++ arduino-jeopardy-buttons.ino
@@ -1,10 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+struct Player {
+  uint8_t buttonPin;
+  uint8_t ledPin;
+};
 
+const Player players[] = {{2, 3}, {4, 5}, {7, 6}};
+
+const uint8_t resetButton = 8;
+
+struct {
+  bool isActive;
+  size_t player;
+} activePlayer = {false, 0};
+
+void setup() {
+  for (const Player &player : players) {
+    pinMode(player.ledPin, OUTPUT);
+    pinMode(player.buttonPin, INPUT);
+  }
+  pinMode(resetButton, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Reset
+  if (digitalRead(resetButton)) {
+    activePlayer = {false, 0};
+    // Reset all LEDs
+    for (const Player &player : players) {
+      digitalWrite(player.ledPin, LOW);
+    }
+  }
+  size_t i = 0;
+  for (const Player &player : players) {
+    if (activePlayer.isActive) {
+      break;
+    }
+    if (digitalRead(player.buttonPin)) {
+      activePlayer = {true, i};
+      digitalWrite(player.ledPin, HIGH);
+    }
+    i++;
+  }
 }
-
